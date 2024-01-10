ecd3f6950540f9346f168440f7c5a1e8273ef641
john2417 <john2417@naver.com>
2024-01-11 01:32:36 +0900

    change

--- book.py
+++ book.py
@@ -2,6 +2,13 @@ class book (object):
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
     def greeting(self):
         print("Book name : " + self.title)
-        print("Author : " + self.author)
+        print("Autor : " + self.author)
+
+    def setTitle(self, title):
+        self.title = title
+
+    def toString(self):
+        return self.title + "," + self.author + '/n'
