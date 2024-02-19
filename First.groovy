3a2ddf457b7edc92a3d386df0bcc2fe1207f9064
john2417 <john2417@naver.com>
2024-02-19 15:24:12 +0900

    groovy add

---
+++ First.groovy
@@ -0,0 +1,25 @@
+pipline{
+    agent any
+    stages{
+        stage("Build"){
+            steps{
+                echo'building the application...'
+            }
+        }
+        stage("test"){
+            steps{
+                echo'testing the application...'
+            }
+        }
+        stage("deploy"){
+            steps{
+                echo'deploying the application...'
+            }
+        }
+
+
+    }
+
+
+
+}
